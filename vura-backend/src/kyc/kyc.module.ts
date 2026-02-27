3250e9cb2971816afb624f78fe0417db4397ff33
RealSSA <realssa@realssa.com>
2026-02-27 18:46:26 +0100

    Fix KYCModule: add NINService and QoreIDService providers

--- vura-backend/src/kyc/kyc.module.ts
+++ vura-backend/src/kyc/kyc.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { KYCController } from './kyc.controller';
 import { BVNService } from './bvn.service';
+import { NINService } from './nin.service';
+import { QoreIDService } from './qoreid.service';
 import { PrismaService } from '../prisma.service';
 
 @Module({
   controllers: [KYCController],
-  providers: [BVNService, PrismaService],
-  exports: [BVNService],
+  providers: [BVNService, NINService, QoreIDService, PrismaService],
+  exports: [BVNService, NINService, QoreIDService],
 })
 export class KYCModule {}
